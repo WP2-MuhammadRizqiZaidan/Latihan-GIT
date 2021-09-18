cec45fbeca18ab2727c713431f2c0cbc05aa05af
WP2-MuhammadRizqiZaidan <90933451+WP2-MuhammadRizqiZaidan@users.noreply.github.com>
2021-09-18 20:42:15 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+}
+        echo "<h1>Pekenalkan/h1>";
+        echo"Nama saya Muhammad Rizqi Zaidan
+             Saya tinggal di daerah Cibinong,Bogor
+             Olahraga yang saya sukai adalah
+             Sepak Bola";
